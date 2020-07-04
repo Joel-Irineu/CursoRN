5cb6debcebe092511bf9cba4b750498e2570525f
Joel Irineu <irineu.joel01@gmail.com>
2020-07-04 16:04:52 -0300

    interpolando animações

--- src/index.js
+++ src/index.js
@@ -13,74 +13,31 @@ import {
 
 
 export default function Main(){
-    const [largA, setLargA] = useState(new Animated.Value(150))
-    const [altA, setAltA] = useState(new Animated.Value(0))
-    const [opacityA, setOpacityA] = useState(new Animated.Value(0))
+    const [largA, setLargA] = useState(new Animated.Value(0))
+    const [altA, setAltA] = useState(new Animated.Value(50))
+    const [opacityA, setOpacityA] = useState(new Animated.Value(1))
     const [animation, setAnimation] = useState(false)
 
-    function carregar(){
-        if(animation === false){
-            Animated.parallel([
-                Animated.timing(
-                    altA,{
-                        toValue: 400,
-                        duration: 2000,
-                        useNativeDriver: false,
-                    }
-                ),
-                Animated.timing(
-                    opacityA,{
-                        toValue: 1,
-                        duration: 2000,
-                        useNativeDriver: false,
-                    }
-                )
-            ]).start()
-
-            setAnimation(true)
-
-        }else{
-            Animated.parallel([
-                Animated.timing(
-                    altA,{
-                        toValue: 0,
-                        duration: 2000,
-                        useNativeDriver: false,
-                    }
-                ),
-                Animated.timing(
-                    opacityA,{
-                        toValue: 0,
-                        duration: 2000,
-                        useNativeDriver: false,
-                    }
-                )
-            ]).start()
-
-            setAnimation(false)
+    Animated.timing(
+        largA,{
+            toValue: 100,
+            duration: 5000,
+            useNativeDriver: false
         }
-    }
+    ).start()
     
+    let porcA = largA.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['0%', '100%'],
+    })
+
     return(
+
         <View style={styles.container}>
             <StatusBar/>
-            <View style={{height: 80, alignItems: 'center', 
-            justifyContent: 'center', flexDirection: 'row', backgroundColor: '#4169e1'}}>
-                <TouchableOpacity
-                    onPress={carregar}
-                    style={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}
-                >
-                    <Text style={{fontSize: 20, color: '#eee'}}>Gerar Grafico</Text>
-                </TouchableOpacity>
-
-            </View>
-            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center'}}>
-                <Text>Vendas</Text>
-                <Animated.View style={{width: largA, height: altA, backgroundColor: '#4169e1', justifyContent: 'center', opacity: opacityA}}>
-                    <Text style={styles.title}>80%</Text>
-                </Animated.View>
-            </View>
-
+            <Animated.View style={{width: porcA, height: altA, backgroundColor: '#4169e1', justifyContent: 'center'}}>
+                <Text style={styles.title}>carregando...</Text>
+            </Animated.View>
         </View>
     )
 }
@@ -88,9 +45,10 @@ export default function Main(){
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        justifyContent: 'center',
     },
     title:{
-        fontSize: 20,
+        fontSize: 10,
         fontWeight: 'bold',
         color: '#fff',
         textAlign: 'center'
